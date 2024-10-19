375798f5ab1d36bb7a376e1ec10f43685e3e96fe
jnzhao3 <jennifer.zhao@emotivohealth.com>
2024-10-19 08:56:30 -0700

    webvoyager

---
+++ WebVoyager/run_local.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python -u run.py \
+    --test_file ./data/tasks_test.jsonl \
+    --api_key YOUR_OPENAI_API_KEY \
+    --headless \
+    --max_iter 15 \
+    --max_attached_imgs 3 \
+    --temperature 1 \
+    --fix_box_color \
+    --seed 42
