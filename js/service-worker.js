65ba920a3b09f992245f06aeda1d076f7c49a162
prestonfu <prestonfu@berkeley.edu>
2024-10-19 08:53:43 -0700

    just pushing things over

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,3 +1,14 @@
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason.search(/install/g) === -1) {
+      return
+  }
+  chrome.tabs.create({
+      url: chrome.runtime.getURL("welcome.html"),
+      active: true
+  })
+})
+
 chrome.sidePanel
           .setPanelBehavior({ openPanelOnActionClick: true })
           .catch((error) => console.error(error));
+
